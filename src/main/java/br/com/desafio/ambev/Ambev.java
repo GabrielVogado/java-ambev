e3863f26fc17f3ad83f620d6f53e20949c1b5b22
Gabriel Vogado <gabriel.vogado@gmail.com>
2024-12-11 22:31:27 -0300

    Arquivos de Configuração - Escopo de projeto

---
+++ src/main/java/br/com/desafio/ambev/Ambev.java
@@ -0,0 +1,13 @@
+package br.com.desafio.ambev;
+
+/**
+ * Hello world!
+ *
+ */
+public class Ambev
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
